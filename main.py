cdb48b69c3f99b34bfdf2c4f2f8e30cbafe03d11
Mirjamol Mirislomov <alpholmon@gmail.com>
2025-12-15 15:17:01 +0500

    set

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-print("Github uchun birinchi kod")
+import os
+os.system("cls")
 
-for i in range(10):
-  print(i)
-  
-a = 1
-while a < 100:
-  print(a) 
+# set1 = {1,2,3,4,10,11,15}
+# set1.add("salom")
+# print(set1)
+
+
+# set1 = {1,2,3,4,10,11,15}
+# set1.remove(10)
+# print(set1)
+
+# set1 = {5,6,1,2,3,4,10,11,15}
+# set1.pop()
+# set1.pop()
+# print(set1)
+
+# set1 = {1,2,3,4,10,11,15}
+# set1.discard(20)
+# print(set1)
+
+# set1 = {1,2,3,4,10,11,15}
+# set1.clear()
+# print(set1)
+
+
+# set1 = {1,2,3,4,10,11,15}
+# set2 = set1.copy()
+# set2.add(14)
+# print(set2)
+# print(set1)
+
+
+
+# set1 = {1,2,3,4,10,11,15}
+# lst = [6,7,8,9]
+# set1.update(lst)
+# print(set1)
+
+
+# set1 = {1,2,3,4,10,11,15}
+# lst = [6,7,8,9]
+# set2 = set1.union(lst)
+# print(set1)
+# print(set2)
